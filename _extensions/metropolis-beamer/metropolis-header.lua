41dbd68c6f376df5419081c770fcb7a49028c210
shafayetShafee <sshafee@isrt.ac.bd>
2024-05-04 00:22:21 +0600

    created the extension

---
+++ _extensions/metropolis-beamer/metropolis-header.lua
@@ -0,0 +1,85 @@
+--[[
+MIT License
+
+Copyright (c) 2024 Shafayet Khan Shafee
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
+local str = pandoc.utils.stringify
+
+local function ensureHtmlDeps()
+  quarto.doc.add_html_dependency({
+    name = "metropolis-beamer",
+    version = "1.0.0",
+    scripts = {
+        { path = "resources/js/add_header.js", attribs = {defer = "true"}}
+      }
+  })
+end
+
+if quarto.doc.is_format('revealjs') then
+  -- Ensuring the dependencies got loaded before proceeding
+  ensureHtmlDeps()
+  
+  function Span(el)
+      local style = el.attributes["style"] or ""
+      local color = el.attributes['color']
+      local bg_color = el.attributes['bg']
+    
+      if color then
+        local fcolor = "color: " .. color .. ";"
+        el.attributes['color'] = nil
+        style = style .. ";" .. fcolor 
+    end
+    if bg_color then
+        local bcolor = "background-color: " .. bg_color .. ";"
+        el.attributes['bg-color'] = nil
+        style = style .. ";" .. bcolor 
+    end
+    
+    el.attributes['style'] = style
+    return el
+  end
+    
+  function Pandoc(doc)
+    local blocks = doc.blocks
+    local str = pandoc.utils.stringify
+    local meta = doc.meta
+    -- make divs structure for holding text and logo.
+    local header_logo = meta['header-logo'] and str(meta['header-logo']) or ""
+    local header_logo_link = meta['header-logo-link'] and str(meta['header-logo-link']) or ""
+    local header_img = pandoc.Div(pandoc.Image("", header_logo, ""), {class = "header-logo"})
+    
+    if header_logo_link ~= "" then
+      header_img.attributes['header-logo-link'] = header_logo_link
+    end
+    
+    local header_title = pandoc.Div({pandoc.Para(' ')} ,{class = "header-title"})
+
+    local div = pandoc.Div(
+      {
+        header_title,
+        header_img
+      }, 
+      {class = 'reveal-header'})
+    table.insert(blocks, div)
+    return doc
+  end
+end
---
+++ _extensions/metropolis-beamer/resources/js/add_header.js
@@ -0,0 +1,109 @@
+/**
+ * reveal-header
+ * A filter that adds header text and logo.
+ * 
+ * MIT License
+ * Copyright (c) 2023-2024 Shafayet Khan Shafee.
+ */
+
+function header() {
+  
+  // add the header structure as the firstChild of div.reveal-header
+  function add_header() {
+    let header = document.querySelector("div.reveal-header");
+    let reveal = document.querySelector(".reveal");
+    reveal.insertBefore(header, reveal.firstChild);
+    
+    let header_title_p_placeholder = document.querySelector('div.header-title > p');
+    let header_title_h2_placeholder = document.createElement('h2');
+    header_title_p_placeholder.replaceWith(header_title_h2_placeholder);
+    
+    logo_img = document.querySelector('.header-logo > img');
+    if (logo_img.getAttribute('src') == null) {
+      if (logo_img?.getAttribute('data-src') != null) {
+        logo_img.src = logo_img?.getAttribute('data-src') || "";
+        logo_img.removeAttribute('data-src'); 
+      };
+    };
+  };
+  
+  
+  function make_h2_title() {
+    let h2_text = Reveal.getCurrentSlide().getAttribute('data-h2-text');
+    let header_title_placeholder = document.querySelector('div.header-title > h2');
+    header_title_placeholder.innerText = h2_text;
+    
+    let header_div = document.querySelector('div.reveal-header');
+    
+    if(Reveal.getCurrentSlide().id == 'title-slide' ||
+       Reveal.getCurrentSlide().classList.contains('title-slide') || h2_text == ''
+       ) {
+      header_div.style.visibility = 'hidden';
+    } else {
+      header_div.style.visibility = 'visible';
+      header_title_placeholder.style.color = 'white';
+    };
+  };
+  
+  
+  function linkify_logo(logo, href) {
+    const logo_cloned = logo.cloneNode(true);
+    const link = document.createElement('a');
+    link.href = href;
+    link.target = '_blank';
+    link.appendChild(logo_cloned);
+    logo.replaceWith(link);
+  };
+    
+  function get_clean_attrs(elem, attrName) {
+    let attrVal = elem.getAttribute(attrName);
+    if (attrVal != null) {
+     elem.removeAttribute(attrName); 
+    }
+    return attrVal;
+  };
+  
+  
+  if (Reveal.isReady()) {
+    add_header();
+    
+    const slides = Reveal.getSlides();
+    slides.forEach(slide => {
+      const h2Element = slide.querySelector('h2');
+      
+      if (h2Element) {
+        h2Element.style.display = 'none';
+        const h2Text = h2Element.textContent;
+        slide.setAttribute('data-h2-text', h2Text);
+      } else {
+        slide.setAttribute('data-h2-text', '');
+      };
+  });
+    
+    make_h2_title();
+    
+    /*************** linkifying the header and footer logo ********************/
+    const header_logo = document.querySelector('div.header-logo');
+    if (header_logo != null) {
+      const header_logo_link = get_clean_attrs(header_logo, 'data-header-logo-link');
+      const footer_logo_link = get_clean_attrs(header_logo, 'data-footer-logo-link');
+      
+      if (header_logo_link != null) {
+        const header_logo_img = document.querySelector('div.header-logo img');
+        linkify_logo(header_logo_img, header_logo_link);
+      };
+      
+    };
+    /****************************** END ***************************************/
+    
+    Reveal.on( 'slidechanged', event => {
+      make_h2_title();
+    });
+    
+  };
+};
+
+
+window.addEventListener("load", (event) => {
+  header();
+});
